435cc9975db81082d7482c4b0ce0ef67caceeac0
Waheed Khan <wahidXX@xxx.com>
2025-09-29 14:28:10 +0530

    Added swagger in controller layer with security

--- src/main/java/com/medico/webapp/config/OpenApiConfig.java
+++ src/main/java/com/medico/webapp/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.medico.webapp.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,7 @@ public class OpenApiConfig {
 
   @Bean
   public OpenAPI openApiConf() {
+    final String securitySchemeName = "bearerAuth";
     return  new OpenAPI().info(
       new Info()
         .title("Ecommerce WebService As RestFull APIs")
@@ -22,6 +26,12 @@ public class OpenApiConfig {
             .email("admin@gmail.com")
             .url("http://localhost/contactus")
         )
-    );
+    ).addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+      .components(new Components().addSecuritySchemes(securitySchemeName,
+        new SecurityScheme()
+          .name(securitySchemeName)
+          .type(SecurityScheme.Type.HTTP)
+          .scheme("bearer")
+          .bearerFormat("JWT")));
   }
 }
--- src/main/java/com/medico/webapp/controller/ProductsController.java
+++ src/main/java/com/medico/webapp/controller/ProductsController.java
@@ -3,15 +3,17 @@ package com.medico.webapp.controller;
 import com.medico.webapp.dto.Response;
 import com.medico.webapp.entity.Product;
 import com.medico.webapp.services.ProductService;
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@Tag(name="Products", description = "Operations related to products")
 public class ProductsController {
 
   @Autowired
@@ -19,24 +21,44 @@ public class ProductsController {
 
   // GET all products : Fetch all products data
   @GetMapping()
+  @Operation(
+    summary = "Get All products",
+    description = "Fetch all product details",
+    security = { @SecurityRequirement(name = "bearerAuth") } // Requires JWT token
+  )
   public  ResponseEntity<Response> getProducts() {
     return productService.getAll();
   }
 
   // Get one product by id
   @GetMapping("/{id}")
+  @Operation(
+    summary = "Get product by ID",
+    description = "Fetch product details by product ID",
+    security = { @SecurityRequirement(name = "bearerAuth") } // Requires JWT token
+  )
   public ResponseEntity<Response> getProduct(@PathVariable String id) {
    return productService.getById(id);
   }
 
   // Add new product
   @PostMapping()
+  @Operation(
+    summary = "Create a new product",
+    description = "Add a new product to the catalog",
+    security = { @SecurityRequirement(name = "bearerAuth") }
+  )
   public ResponseEntity<Response> addProduct(@RequestBody Product product) {
     return productService.save(product);
   }
 
   // Update a product
   @PutMapping("/{id}")
+  @Operation(
+    summary = "Update product by ID",
+    description = "Update product details by product ID",
+    security = { @SecurityRequirement(name = "bearerAuth") } // Requires JWT token
+  )
   public ResponseEntity<Response> updateProduct(@PathVariable String id, @RequestBody Product product) {
     product.setId(id);
     return productService.update(product);
@@ -44,6 +66,11 @@ public class ProductsController {
 
   // Delete a product
   @DeleteMapping("/{id}")
+  @Operation(
+    summary = "Delete product by ID",
+    description = "Delete product details by product ID",
+    security = { @SecurityRequirement(name = "bearerAuth") } // Requires JWT token
+  )
   public ResponseEntity<Response> deleteProduct(@PathVariable String id) {
     return productService.delete(id);
   }
